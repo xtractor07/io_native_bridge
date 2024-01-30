40dc7994231dc800e297796348a81be07b3e762e
xtractor07 <tryy.m3@gmail.com>
2024-01-30 11:26:31 +0530

    CustomModule : BridgeInitated

---
+++ ios/CustomModule.m
@@ -0,0 +1,8 @@
+//
+//  CustomModule.m
+//  MyNativeModule
+//
+//  Created by Kumar Aman on 30/01/24.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/CustomModule.swift
@@ -0,0 +1,8 @@
+//
+//  CustomModule.swift
+//  MyNativeModule
+//
+//  Created by Kumar Aman on 30/01/24.
+//
+
+import Foundation
